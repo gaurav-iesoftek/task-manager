4d2697c12bffb710cc130fcf0c5cbf69c43c0a8c
DJ <gk22719@gmail.com>
2025-07-07 11:22:11 +0530

    initial commit

---
+++ task-manager-ui/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <main className="flex flex-col items-center justify-center w-full flex-1 text-center">
+        <h1>Task Manager</h1>
+      </main>
+    </div>
+  );
+}
